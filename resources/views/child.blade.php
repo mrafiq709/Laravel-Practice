d553a1a17ec6582bdc14e73d1b7af8a5919f3ac8
mrafiq709 <mrafiq709@gmail.com>
2019-09-16 13:05:11 +0700

    update

--- resources/views/child.blade.php
+++ resources/views/child.blade.php
@@ -3,17 +3,138 @@
 @section('title', 'Laravel 6 Blade Template')
 
 @section('sidebar')
-    @parent
-    <p>This is appended to the master sidebar.</p>
+@parent
+  <p>This is appended to the master sidebar.</p>
 @endsection
 
 @section('content')
-    <p>Hi, {{$name}}</p>
+  <p>Hi, {{$records['name']}}</p>
 
-    @alert(['type' => 'success', 'icon' => 'check'])
-      Success !
-    @endalert
+<!-- alert compnent. Also need to add the component in: Providers -> AppServiceProvider,php -> boot() method -->
+@alert(['type' => 'success', 'icon' => 'check'])
+  Success !
+@endalert
 
-    The current UNIX timestamp is {{ time() }}.
+<!-- current time -->
+The current UNIX timestamp is {{ time() }}.
+
+<br/><br/>
+
+<!-- if else statement -->
+@if (count($records) === 1)
+  I have one record!
+@elseif (count($records) > 1)
+  I have multiple records!
+@else
+  I don't have any records!
+@endif
+
+<br/><br/>
+
+<!-- switch statement -->
+@switch(count($records))
+    @case(1)
+        First case...
+        @break
+
+    @case(2)
+        Second case...
+        @break
+
+    @default
+        Default case...
+@endswitch
+
+<br/><br/>
+
+<!-- for loop statement -->
+@for ($i = 0; $i < 10; $i++)
+    The current value is {{ $i }}
+    <br/>
+@endfor
+
+<br/>
+
+<!-- foreach statement -->
+@foreach ($records as $record)
+    <h6>{{ $record }}</h6>
+@endforeach
+
+<!-- PHP code -->
+@php
+  $users = ['Rafiq', 'Towhid', 'Ashraf', 'Tammum', 'ISTVN'];
+@endphp
+
+@foreach ($users as $user)
+    @if ($user == 'Towhid')
+        @continue
+    @endif
+
+    @if ($user == 'ISTVN')
+        @break
+    @endif
+
+    <li>{{ $user }}</li>
+
+@endforeach
+
+<br/>
+
+<!-- loop Variable / Loop first index or last index -->
+@foreach ($users as $user)
+    @if ($loop->first)
+        This is the first iteration.
+    @endif
+
+    @if ($loop->last)
+        This is the last iteration.
+    @endif
+
+    <li>{{ $user }}</li>
+@endforeach
+
+<!-- PHP code -->
+@php
+  $records = [];
+@endphp
+
+<!-- forelase statement -->
+@forelse ($records as $record)
+    <li>{{ $record }}</li>
+@empty
+    <p>No users</p>
+@endforelse
+
+<!-- PHP code -->
+@php
+  $cnt = 0;
+@endphp
+
+<!-- while loop statement -->
+@while ($cnt < 5)
+    <h6>I'm looping forever.</h6>
+
+    <!-- PHP code -->
+    @php
+      $cnt++;
+    @endphp
+@endwhile
+
+<div class="row alert">
+  <span class="closebtn" onclick="this.parentElement.style.display='none';">&times;</span>
+
+  <div class="col s1" style="background:black">
+    <div class="material-icons">error_outline</div>
+  </div>
+  <div class="col s11">
+    <strong>Danger!</strong> <br/>Indicates a dangerous or potentially negative action.
+     <br/>Indicates a dangerous or potentially negative action.
+      <br/>Indicates a dangerous or potentially negative action.
+  </div>
+</div>
+
+<br/>
+<div class="line"></div>
+<h5><a href="https://laravel.com/docs/6.0/blade#displaying-data">References</a></h5>
 
 @endsection
