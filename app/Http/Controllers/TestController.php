f058bc6bac132f6ddab287428e4916faf3918a20
rafiq <mrafiq709@gmail.com>
2020-07-19 14:32:17 +0000

    PEAR rule set tested

--- app/Http/Controllers/TestController.php
+++ app/Http/Controllers/TestController.php
@@ -1,12 +1,41 @@
 <?php
+/**
+ * Template File Doc Comment
+ * 
+ * PHP version 7
+ *
+ * @category Template_Class
+ * @package  Template_Class
+ * @author   Author <author@domain.com>
+ * @license  https://opensource.org/licenses/MIT MIT License
+ * @link     http://localhost/
+ */
 
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
 
+/**
+ * Template Class Doc Comment
+ * 
+ * Template Class
+ * 
+ * @category Template_Class
+ * @package  Template_Class
+ * @author   Author <author@domain.com>
+ * @license  https://opensource.org/licenses/MIT MIT License
+ * @link     http://localhost/
+ */
 class TestController extends Controller
 {
-    private function testTest()
+    /**
+     * Define template file.
+     * 
+     * @param string $ok test
+     * 
+     * @return void
+     */
+    public function testTest($ok)
     {
     }
 }
