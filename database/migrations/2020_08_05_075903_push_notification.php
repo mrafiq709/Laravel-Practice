9124956dfd9487550cbd9190f2042294018591a7
rafiqul <rafiqul@scuti.asia>
2020-08-05 15:21:13 +0700

    initial commit

---
+++ database/migrations/2020_08_05_075903_push_notification.php
@@ -0,0 +1,40 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class PushNotification extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('push_notifications', function (Blueprint $table) {
+            $table->bigIncrements('id');
+            $table->bigInteger('creator_id')->unsigned();
+            $table->bigInteger('sender_id')->unsigned()->nullable();
+            $table->foreign('creator_id')->references('id')->on('users');
+            $table->foreign('sender_id')->references('id')->on('users');
+            $table->string('title', 500)->nullable();
+            $table->text('message');
+            $table->json('data')->nullable();
+            $table->enum('platform', ['all', 'android', 'ios'])->default('all');
+            $table->json('response')->nullable();
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('push_notifications');
+    }
+}
