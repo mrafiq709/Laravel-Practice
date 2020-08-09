5074cd390404c08a5c394ff24772fdb552da0826
rafiq <mrafiq709@gmail.com>
2020-08-09 14:17:53 +0000

    update phpcs rulesets and git-hooks command from composer.json

--- app/Http/Controllers/TestController.php
+++ app/Http/Controllers/TestController.php
@@ -1,43 +1,25 @@
 <?php
-/**
- * Template File Doc Comment
- * 
- * PHP version 7
- *
- * @category Template_Class
- * @package  Template_Class
- * @author   Author <author@domain.com>
- * @license  https://opensource.org/licenses/MIT MIT License
- * @link     http://localhost/
- */
 
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
 
 /**
- * Template Class Doc Comment
- * 
- * Template Class
- * 
- * @category Template_Class
- * @package  Template_Class
- * @author   Author <author@domain.com>
- * @license  https://opensource.org/licenses/MIT MIT License
- * @link     http://localhost/
+ * Hi I am rafiq
  */
 class TestController extends Controller
 {
-
     /**
+     * Hi ok
      * 
+     * @var mix $tt
      */
-    private $_tt;
+    private $tt;
 
     /**
-     * Define template file.
+     * Ok hi
      * 
-     * @param string $ok test
+     * @param int $ok hfhgf
      * 
      * @return void
      */
