ec97b2629cb8d0f00b22264f2d4caa979c3fb6fe
rafiq <rafiqul709@scuti.asia>
2021-02-24 17:48:08 +0700

    add some code

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -43,8 +43,37 @@
             </span>
             </span>
         </div>
+
+        <!-- Blade component -->
         @php $message = "something is going wrong !"; $title = "Holy smokes!"; @endphp
+        <div class="mr-300">
         <x-alert type="red" :title="$title" :message="$message"/>
+        </div>
         <x-modern-with-badge type="indigo" :title="$title" :message="$message"/>
+
+        <div class="py-8 px-8">
+            <div class="py-8 px-8 max-w-sm mx-auto bg-white rounded-xl shadow-md space-y-2 sm:py-4 sm:flex sm:items-center sm:space-y-0 sm:space-x-6">
+                <img class="block mx-auto h-24 rounded-full sm:mx-0 sm:flex-shrink-0" src="ft34.jpg" alt="Woman's Face">
+                <div class="text-center space-y-2 sm:text-left">
+                    <div class="space-y-0.5">
+                    <p class="text-lg text-black font-semibold">
+                        Erin Lindford
+                    </p>
+                    <p class="text-gray-500 font-medium">
+                        Product Engineer
+                    </p>
+                    </div>
+                    <button class="px-4 py-1 text-sm text-purple-600 font-semibold rounded-full border border-purple-200 hover:text-white hover:bg-purple-600 hover:border-transparent focus:outline-none focus:ring-2 focus:ring-purple-600 focus:ring-offset-2">Message</button>
+                </div>
+            </div>
+        </div>
+
+        <div class="rounded-t-xl overflow-hidden bg-gradient-to-r from-indigo-50 to-indigo-100 p-10">
+            <div class="group px-6 py-5 max-w-full mx-auto w-72 border border-indigo-500 border-opacity-25 cursor-pointer rounded-lg select-none overflow-hidden space-y-1 hover:bg-white hover:shadow-lg hover:border-transparent">
+                <p class="font-semibold text-lg text-indigo-600 group-hover:text-gray-900">New Project</p>
+                <p class="text-indigo-500 group-hover:text-gray-500">Create a new project from a variety of starting templates.</p>
+            </div>
+        </div>
+
     </body>
 </html>
